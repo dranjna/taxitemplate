33dd8bb87a71aa802bb4b05a7fad4469779215e7
manveer <d.ranjna96@gmail.com>
2024-07-30 09:52:25 +0530

    taxitemplate

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navbarToggler = document.getElementById('navbar-toggler');
+// slick slider code
+$(document).ready(function () {
+    $('.carousel > .row').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        rows: 2,
+        prevArrow: "<img class='a-left control-c prev slick-prev' src='./assets/images/Button.png'>",
+        nextArrow: "<img class='a-right control-c next slick-next' src='./assets/images/Button (1).png'>",
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ],
+    })
 
-    navbarToggler.addEventListener('click', function () {
-        this.classList.toggle('collapsed');
-    });
+    $('.carousel2 > .row').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: "<img class='a-left control-c prev slick-prev' src='./assets/images/Button.png'>",
+        nextArrow: "<img class='a-right control-c next slick-next' src='./assets/images/Button (1).png'>",
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ],
+    })
 });
